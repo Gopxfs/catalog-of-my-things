2534926759246786dc20f082f505e5ce32951407
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-05 16:59:15 -0300

    create author class

---
+++ author.rb
@@ -0,0 +1,17 @@
+require 'item'
+
+class Author < Item
+attr_reader :id, :items
+attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @id = id || Random.rand(1...10_000)
+    @items = []
+  end
+
+  def add_item(item)
+    item.add_author(self)
+  end
+end
