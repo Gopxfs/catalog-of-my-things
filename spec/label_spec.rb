1e7e99f28989b55c823bc72295c179c7467814ae
Gabriel <gopxfs@hotmail.com>
2022-09-05 19:22:20 -0300

    Add unit tests to class Label

---
+++ spec/label_spec.rb
@@ -0,0 +1,21 @@
+require_relative './spec_helper'
+
+describe Label do
+  before :each do
+    @label = Label.new(1, 'title', 'color')
+  end
+
+  describe '#new' do
+    it 'receives 3 parameters and returns a Label object' do
+      expect(@label).to be_an_instance_of Label
+    end
+  end
+
+  describe '#add_item' do
+    it 'adds an item to the items array' do
+      item = Item.new(1, '05/09/2022')
+      @label.add_item(item)
+      expect(@label.items[0]).to eql item
+    end
+  end
+end
