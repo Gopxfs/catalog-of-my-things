acd97d3062cb1fed41091a13afc0c19c0a1b1fc9
Gabriel <gopxfs@hotmail.com>
2022-09-05 15:24:07 -0300

    Fix linter errors

--- item.rb
+++ item.rb
@@ -2,12 +2,13 @@ require 'date'
 
 class Item
   attr_accessor :publish_date
+
   def initialize(id, publish_date, archived: false)
     @id = id
-    @publish_date = Date.parse(publish_date) #publish_date should be a string = 'DD-MM-YYYY' or 'DD/MM/YYYY'
+    @publish_date = Date.parse(publish_date) # publish_date should be a string = 'DD-MM-YYYY' or 'DD/MM/YYYY'
     @archived = archived
   end
-  
+
   def add_genre(genre)
     @genre = genre
   end
@@ -21,15 +22,12 @@ class Item
   end
 
   def can_be_archived?
-    if DateTime.now.year - @publish_date.year > 10
-      return true
-    end
+    return true if DateTime.now.year - @publish_date.year > 10
+
     false
   end
 
   def move_to_archive
-    if can_be_archived?
-      @archived = true
-    end
+    @archived = true if can_be_archived?
   end
 end
