93328a88526e886d0ce27b02bfc33cbe073abcec
masangana <alexmasangana@gmail.com>
2022-09-05 22:11:57 +0300

    Add vue files
    -- create an App.rb file for main function
    -- Create display_menu.rb for the main menu

--- main.rb
+++ main.rb
@@ -3,8 +3,6 @@ require_relative './display_menu'
 
 def main
   app = App.new
-  Dir.exist?('./data') ? app.read_files : nil
-  Dir.exist?('./data') ? app.read_files2 : nil
   loop do
     display_menu(app)
   end
