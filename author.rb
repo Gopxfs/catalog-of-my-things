0d9081675ee46e07f76dd5664f0aa545ab4c95ef
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-05 17:06:43 -0300

    fix linter errors

--- author.rb
+++ author.rb
@@ -1,10 +1,11 @@
-require 'item'
+require_relative './item'
 
 class Author < Item
-attr_reader :id, :items
-attr_accessor :first_name, :last_name
+  attr_reader :id, :items
+  attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
+    super()
     @first_name = first_name
     @last_name = last_name
     @id = id || Random.rand(1...10_000)
