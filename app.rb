61a7f2f6fa66cc32f0eeb394e2811583859ee1ae
masangana <alexmasangana@gmail.com>
2022-09-05 22:13:14 +0300

    Initialize an simple app

---
+++ app.rb
@@ -0,0 +1,14 @@
+
+class App
+    attr_accessor :books, :music_albums, :games, :authors, :labels, :genres
+
+    def initialize
+        @books = []
+        @music_albums = []
+        @games = []
+        @authors = []
+        @labels = []
+        @genres = []
+    end
+
+end
