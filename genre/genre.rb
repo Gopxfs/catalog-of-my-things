3c00d70f282ae5662bc304bee2cff3ace2281caf
masangana <alexmasangana@gmail.com>
2022-09-06 02:44:59 +0300

    Fix linters errors

--- genre/genre.rb
+++ genre/genre.rb
@@ -2,19 +2,19 @@ require 'date'
 require_relative '../item'
 
 class Genre < Item
-    attr_accessor :name, :items
-    attr_reader :id, :publish_date
+  attr_accessor :name, :items
+  attr_reader :id, :publish_date
 
-    def initialize(name, publish_date)
-        super(id = Random.rand(1..10_000))
-        @name = name
-        @publish_date = Date.parse(publish_date)
-        @id = id
-        @items = []
-    end
+  def initialize(name, publish_date)
+    super(id = Random.rand(1..10_000))
+    @name = name
+    @publish_date = Date.parse(publish_date)
+    @id = id
+    @items = []
+  end
 
-    def add_item(item)
-        (@items << item) unless @items.include?(item)
-        item.add_genre(self)
-    end
+  def add_item(item)
+    (@items << item) unless @items.include?(item)
+    item.add_genre(self)
+  end
 end
--- music/music_methodes.rb
+++ music/music_methodes.rb
@@ -1,28 +1,27 @@
 require_relative './music_album'
 
 def list_music
-    @music_list.each_with_index do |music, index|
-        puts "(#{index}) ID: #{music.id}, Publish Date: #{music.publish_date}, On Spotify: #{music.on_spotify}, Archived: #{music.archived}"
-    end
-    end
+  @music_list.each_with_index do |music, index|
+    puts "(#{index}) ID: #{music.id}, Publish Date: #{music.publish_date}, On Spotify: #{music.on_spotify}, Archived: #{music.archived}" # rubocop:disable Layout/LineLength
+  end
 end
 
 def create_music
-    puts 'Please enter the publish date'
-    publish_date = gets.chomp
-    puts 'Is it on Spotify? (true/false)'
-    on_spotify = gets.chomp
-    music = MusicAlbum.new(publish_date, on_spotify)
-    @music_list << music
+  puts 'Please enter the publish date'
+  publish_date = gets.chomp
+  puts 'Is it on Spotify? (true/false)'
+  on_spotify = gets.chomp
+  music = MusicAlbum.new(publish_date, on_spotify)
+  @music_list << music
 end
 
 def add_music(publish_date, on_spotify)
-    music = MusicAlbum.new(publish_date, on_spotify)
-    @music_list << music
+  music = MusicAlbum.new(publish_date, on_spotify)
+  @music_list << music
 end
 
 def list_labels
-    @music_list.each_with_index do |music, index|
-        puts "(#{index}) ID: #{music.id}, Label: #{music.label}"
-    end
+  @music_list.each_with_index do |music, index|
+    puts "(#{index}) ID: #{music.id}, Label: #{music.label}"
+  end
 end
