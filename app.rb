91267832cd40057397556338200befb9e33fdd65
Gabriel <gopxfs@hotmail.com>
2022-09-06 00:44:21 -0300

    Add display to list of books

--- app.rb
+++ app.rb
@@ -9,4 +9,15 @@ class App
     @labels = []
     @genres = []
   end
+
+  def book_display
+    if @books.empty?
+     puts "There isn't any book in our catalog."
+     sleep(1)
+    else
+      @books.each do |book|
+        puts "[#{book.id}] '#{book.label.title}' by #{book.author.first_name} #{book.author.last_name}"
+      end
+    end
+  end
 end
