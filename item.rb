97e356aa0f76d90a4a56b593761f6db765a07536
masangana <alexmasangana@gmail.com>
2022-09-06 12:49:52 +0300

    Update item.rb

--- item.rb
+++ item.rb
@@ -1,27 +1,36 @@
 require 'date'
 
 class Item
-  attr_accessor :publish_date
+  attr_reader :id, :genre, :publish_date, :archived, :author, :label
 
-  def initialize(id = Random.rand(1..1000))
+  def initialize(id = Random.rand(1..10_000))
     @id = id
     @genre = nil
-    @author = nil
-    @label = nil
     @publish_date = nil
     @archived = false
+    @author = nil
+    @label = nil
   end
 
   def add_genre(genre)
-    @genre = genre
+    genre.is_a?(Genre) && @genre.nil? && (
+      @genre = genre
+      genre.add_item(self)
+    )
   end
 
   def add_author(author)
-    @author = author
+    author.is_a?(Author) && @author.nil? && (
+      @author = author
+      author.add_item(self)
+    )
   end
 
   def add_label(label)
-    @label = label
+    label.is_a?(Label) && @label.nil? && (
+      @label = label
+      label.add_item(self)
+    )
   end
 
   def can_be_archived?
