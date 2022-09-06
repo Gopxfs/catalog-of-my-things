db2eb1eee63bd57794fc510b38f1bc0e6db6fa32
Gabriel <gopxfs@hotmail.com>
2022-09-06 00:58:53 -0300

    Update book id to be a random number

--- book.rb
+++ book.rb
@@ -3,7 +3,7 @@ require './item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(id, publish_date, publisher, cover_state, archived: false)
+  def initialize(publish_date, publisher, cover_state, id = Random.rand(1..10_000), archived: false)
     super(id, publish_date, archived: archived)
     @publisher = publisher
     @cover_state = cover_state
