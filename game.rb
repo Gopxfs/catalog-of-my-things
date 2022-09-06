6894baa9e24e4a08d9433f95260680a1908a3bc3
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-06 12:18:13 -0300

    reorder structure of parameters and remove Date.parse

--- game.rb
+++ game.rb
@@ -3,10 +3,10 @@ require './item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(id, publish_date, multiplayer, last_played_at, archived: false)
-    super(id, publish_date, archived: archived)
+  def initialize(publish_date, multiplayer, last_played_at, id = Random.rand(1..10_000), archived: false)
+    super(publish_date, id, archived: archived)
     @multiplayer = multiplayer
-    @last_played_at = Date.parse(last_played_at) # last_played_at should be a string = 'DD-MM-YYYY' or 'DD/MM/YYYY'
+    @last_played_at = last_played_at # last_played_at should be a string = 'DD-MM-YYYY' or 'DD/MM/YYYY'
   end
 
   private
