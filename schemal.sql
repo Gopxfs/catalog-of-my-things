6e99a321a30cf6468ad16990a97676603d195b5e
Gabriel <gopxfs@hotmail.com>
2022-09-07 13:47:26 -0300

    Fix id on schema.sql

--- schemal.sql
+++ schemal.sql
@@ -26,7 +26,7 @@ CREATE TABLE label (
 
 -- Create a Game class table
 CREATE TABLE game (
-  id SERIAL PRIMARY KEY ,
+  id SERIAL PRIMARY KEY GENERATED ALWAYS ,
   item_id INT FOREIGN KEY REFERENCES item(id),
   multiplayer VARCHAR,
   last_played_at DATE,
@@ -35,15 +35,15 @@ CREATE TABLE game (
 
 -- Create an Author class table
 CREATE TABLE author (
-	id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY GENERATED ALWAYS,
   item_id INT FOREIGN KEY REFERENCES item(id),
 	first_name VARCHAR,
-	last_name VARCHAR
+	last_name VARCHAR,
 );
 
 -- Create a Music Album class table
 CREATE TABLE music_album (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY GENERATED ALWAYS,
   item_id INT FOREIGN KEY REFERENCES item(id),
   on_spotify BOOLEAN,
   publish_date DATE,
@@ -52,7 +52,7 @@ CREATE TABLE music_album (
 
 -- Create a Genre class table
 CREATE TABLE genre (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY GENERATED ALWAYS,
   item_id INT FOREIGN KEY REFERENCES item(id),
-  name VARCHAR
+  name VARCHAR,
 );
