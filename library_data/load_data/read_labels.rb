201bbe3b79cbb99fbb65b2a9bdc6db89dced08e4
Gabriel <gopxfs@hotmail.com>
2022-09-07 10:58:18 -0300

    Add methods to load labels and link them with their items

--- library_data/load_data/read_labels.rb
+++ library_data/load_data/read_labels.rb
@@ -1,14 +1,29 @@
 require 'json'
+require './label'
 
-def load_labels
-  return [] unless file_exist?
-  return [] if File.zero?('./library_data/data/labels.json')
+def load_labels(books)
+  file = './library_data/data/labels.json'
 
-  ################
+  return [] unless File.exist?(file)
+  return [] if File.zero?(file)
+
+  labels = []
+  labels_data = JSON.parse(File.read(file))
+
+  labels_data.each do |label|
+    new_label = Label.new(label['title'], label['color'], label['id'])
+    load_items(new_label, label['items'], books)
+    labels.push(new_label)
+  end
+  labels
 end
 
-def file_exist?
-  return false unless File.exist?('./library_data/data/labels.json')
-
-  true
+def load_items(label, items, books)
+  items.each do |item|
+    books.each do |book|
+      if book.id == item['item_id'] && book.class.name == item['item_class']
+        label.add_item(book)
+      end
+    end
+  end
 end
