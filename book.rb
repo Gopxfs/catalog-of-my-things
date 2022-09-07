a315fde003f6cce65cb96993576d1e8b90c0eb7d
Gabriel <gopxfs@hotmail.com>
2022-09-06 22:14:17 -0300

    Remove json methods from private

--- book.rb
+++ book.rb
@@ -10,23 +10,23 @@ class Book < Item
     @cover_state = cover_state
     @archived = false
   end
+ 
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'properties' => [publish_date, publisher, cover_state, id]
+    }.to_json(*args)
+  end
 
+  def self.json_create(object)
+    new(*object['properties'])
+  end
+  
   private
 
   def can_be_archived?
     return true if super || cover_state == 'bad'
 
     false
-  end
-  
-  def to_json(*args)
-    {
-      JSON.create_id => self.class.name,
-      'properties' => [publish_date, publisher, cover_state, id, archived]
-    }.to_json(*args)
-  end
-
-  def self.json_create(object)
-    new(*object['properties'])
   end
 end
