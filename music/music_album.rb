99926169ff79d645b546eb932afd068f6b5d10b3
Gabriel <gopxfs@hotmail.com>
2022-09-07 15:07:06 -0300

    Update id syntax

--- music/music_album.rb
+++ music/music_album.rb
@@ -6,8 +6,8 @@ class MusicAlbum < Item
   attr_accessor :on_spotify, :archived, :publish_date
   attr_reader :id, :genre, :author, :label
 
-  def initialize(publish_date, on_spotify)
-    super(publish_date, id = Random.rand(1..10_000))
+  def initialize(publish_date, on_spotify, id = Random.rand(1..10_000))
+    super(publish_date, id)
     @publish_date = Date.parse(publish_date)
     @id = id
     @on_spotify = on_spotify == 'true'
