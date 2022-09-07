e25c205d30a8f245c77e041cbd8f8413928756b1
Gabriel <gopxfs@hotmail.com>
2022-09-07 15:10:56 -0300

    Add #save_items

--- library_data/preserve_data/save_items.rb
+++ library_data/preserve_data/save_items.rb
@@ -1,11 +1,17 @@
 require 'json'
 require_relative './save_books'
 require_relative './save_labels'
+require_relative './save_musics'
+require_relative './save_genres'
+require_relative './save_authors'
 
-def save_items(books, labels)
+def save_items(books, labels, musics, genres, authors)
   check_directory
   save_books(check_file('books'), books)
   save_labels(check_file('labels'), labels)
+  save_musics(check_file('musics'), musics)
+  save_genres(check_file('genres'), genres)
+  save_authors(check_file('authors'), authors)
 end
 
 def check_directory
